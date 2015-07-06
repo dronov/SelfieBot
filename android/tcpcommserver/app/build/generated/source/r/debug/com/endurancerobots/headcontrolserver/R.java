28005f700052a0c92cff1d29f4f06d5a415cfeae
Ilya Petrov <ilya.petrov.m@gmail.com>
2015-07-06 19:21:08 +0300

    Added Bluetooth communication. + External IP finding

--- android/tcpcommserver/app/build/generated/source/r/debug/com/endurancerobots/headcontrolserver/R.java
+++ android/tcpcommserver/app/build/generated/source/r/debug/com/endurancerobots/headcontrolserver/R.java
@@ -10,19 +10,21 @@ package com.endurancerobots.headcontrolserver;
 public final class R {
     public static final class attr {
     }
-    public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+    public static final class dimen {
+        public static final int adrSize=0x7f040000;
     }
     public static final class id {
-        public static final int buttonBackground=0x7f060002;
+        public static final int buttonBackground=0x7f060004;
+        public static final int editArduinoMac=0x7f060002;
         public static final int imageView=0x7f060001;
+        public static final int ipAddr=0x7f060003;
         public static final int text1=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f040000;
+        public static final int main=0x7f030000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f030000;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- android/tcpcommserver/app/src/main/java/com/endurancerobots/headcontrolserver/TcpServer.java
+++ android/tcpcommserver/app/src/main/java/com/endurancerobots/headcontrolserver/TcpServer.java
@@ -3,31 +3,68 @@ package com.endurancerobots.headcontrolserver;
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.Enumeration;
 
 import android.app.Activity;
+
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import at.abraxas.amarino.Amarino;
+
+
 public class TcpServer extends Activity {
-    /** Called when the activity is first created. */
+	private TextView serverIp;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         textDisplay = (TextView) this.findViewById(R.id.text1);
         textDisplay.setText("Server waiting for connections");
+
+		serverIp = (TextView)findViewById(R.id.ipAddr);
+		serverIp.setText("IP:" + getLocalIpAddress());
+
         mt = new MyTask();
         mt.execute();
     }
-    private TextView textDisplay;
+
+	public String getLocalIpAddress()
+	{
+		try {
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress()) {
+						return inetAddress.getHostAddress().toString();
+					}
+				}
+			}
+		} catch (Exception ex) {
+			Log.e("IP Address", ex.toString());
+		}
+		return null;
+	}
+	private String getWifiIp(){
+		WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+		return Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+	}
+
+	private TextView textDisplay;
     private static final int TCP_SERVER_PORT = 1553;
     MyTask mt;
     ServerSocket ss = null;
@@ -46,13 +83,15 @@ public class TcpServer extends Activity {
 			super.onPreExecute();
 			try {
 				ss = new ServerSocket(TCP_SERVER_PORT);
-
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			Log.i("TcpServer", "Server started");
+			Log.i("TcpServer", "Server started.\nHost: " +
+					ss.getLocalSocketAddress().toString());
 			Toast.makeText(getApplicationContext(),
-					"Server started on port: " + TCP_SERVER_PORT, Toast.LENGTH_SHORT).show();
+					"Server started.\nHost: "+
+					ss.getLocalSocketAddress().toString(),
+					Toast.LENGTH_LONG).show();
 		}
 
 		@Override
@@ -69,21 +108,29 @@ public class TcpServer extends Activity {
 		@Override
 		protected void onProgressUpdate(byte[]... values) {
 			super.onProgressUpdate(values);
-			textDisplay.setText(values.toString());
-			switch (values.toString().charAt(0)){
-				case 'w':
+			textDisplay.setText(values[0].toString());
+			switch (values[0][0]) {
+				case 119:
 					textDisplay.setText("UP");
+					Log.i("TcpServer.Control", "UP");
 					break;
-				case 'a':
+				case 97:
 					textDisplay.setText("LEFT");
+					Log.i("TcpServer.Control", "LEFT");
 					break;
-				case 's':
+				case 115:
 					textDisplay.setText("DOWN");
+					Log.i("TcpServer.Control", "DOWN");
 					break;
-				case 'd':
+				case 100:
 					textDisplay.setText("RIGHT");
+					Log.i("TcpServer.Control", "RIGHT");
 					break;
 			}
+			Toast.makeText(getApplicationContext(),
+					"Command:"+values[0][0],
+					Toast.LENGTH_SHORT).show();
+			Amarino.sendDataToArduino(getApplicationContext(), getMac(), 'A', values[0]);
 		}
 
 		@Override
@@ -121,5 +168,10 @@ public class TcpServer extends Activity {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private String getMac() {
+		EditText editText = (EditText)findViewById(R.id.editArduinoMac);
+		return editText.getText().toString();
 	}
 }
